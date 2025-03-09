7296436e378c8fbb5638457ccb9883bbe6a989c0
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-03-09 22:35:29 +0700

    Update query product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,40 +2,18 @@ import Product from '../models/Product.js'
 
 export const index = async (req, res) => {
     try {
-        const products = await Product.find();
+        const { jenis, kategori } = req.query
+        let filter = {}
+
+        if (jenis) filter.jenis = jenis
+        if (kategori) filter.kategori = kategori
+        const products = await Product.find(filter);
         res.json(products)
     } catch (err) {
         res.status(500).json({ message: "Terjadi kesalahan pada server!" })
     }
 }
 
-export const man = async(req, res)=> {
-    try {
-        const { jenis, kategori } = req.query
-        let filter = {}
 
-        
 
-        if (jenis) filter.jenis = jenis; // Tambahkan filter jika ada
-        if (kategori) filter.kategori = kategori;
-       
 
-        let pd = await Product.find(filter);
-        
-
-        res.json(pd);
-    } catch (error) {
-        res.status(500).json({ error: "Terjadi kesalahan saat mengambil data" });
-    }
-    
-    
-}
-
-export const woman = async (req, res) => {
-    try {
-        const productsWoman = await Product.find({jenis: 'woman'})
-        res.json(productsWoman)
-    }catch(err){
-        res.status(500).json({ message: "Terjadi kesalahan pada server!" })
-    }
-}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import {index, woman, man} from '../controllers/productController.js'
+import {index} from '../controllers/productController.js'
 
 const router = express.Router()
 
 router.get('/', index)
-router.get('/man', man)
-router.get('/woman', woman)
+
 
 export default router
