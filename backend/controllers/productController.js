c60e04552dd45468c2c1ef9802a785bb5d7d6ef8
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-03-09 23:49:33 +0700

    Update detailProduk API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,6 +14,22 @@ export const index = async (req, res) => {
     }
 }
 
+export const show = async(req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findById(id)
+
+        if (!product) {
+            return res.status(404).json({ message: "Produk tidak ditemukan!" })
+        }
+
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: "Terjadi kesalahan pada server!" })
+    }
+
+}
+
 
 
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import {index} from '../controllers/productController.js'
+import {index, show} from '../controllers/productController.js'
 
 const router = express.Router()
 
 router.get('/', index)
-
+router.get('/:id', show)
 
 export default router
