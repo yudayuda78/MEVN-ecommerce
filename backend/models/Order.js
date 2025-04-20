9b8f9e0674710761a974bd3aed78b609b0485dc7
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-04-20 22:52:28 +0700

    Create Order.js

---
+++ backend/models/Order.js
@@ -0,0 +1,19 @@
+import mongoose, { mongo } from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    items: [
+        {
+            product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            quantity: Number,
+            price: Number
+        }
+    ],
+    total_price: Number,
+    status: { type: String, default: 'pending' },
+    payment_method: String,
+    created_at: { type: Date, default: Date.now }
+})
+
+const Order = mongoose.model('Order', orderSchema, order)
+export default Order
