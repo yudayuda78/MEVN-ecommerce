bebed2ad01cd6ee47c6c33c092766864c31998f2
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-05-04 22:47:27 +0700

    Update component button cart

---
+++ frontend-ecommerce/src/components/button/Button.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineOptions({ inheritAttrs: false })
+</script>
+
+<template>
+    <button :class="['default-button', $attrs.class]">
+      <slot />
+    </button>
+</template>
+
+
+<style scoped>
+.default-button {
+  background-color: black;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
--- frontend-ecommerce/src/views/Cart.vue
+++ frontend-ecommerce/src/views/Cart.vue
@@ -1,3 +1,19 @@
+<script setup>
+import Button from '../components/button/Button.vue'
+</script>
+
 <template>
+    
+    <Button class="button">-</Button> 
     h1 go
+    <Button class="button">+</Button> 
+    <Button class="button">Checkout</Button> 
+
 </template>
+
+<style scoped>
+.button {
+  margin-top: 90px;
+  
+}
+</style>
