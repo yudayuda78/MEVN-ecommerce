df8964d79ab0e9da13ad4bf628bedbff3a596380
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-05-28 21:14:19 +0700

    Feat : add update title

--- backend/controllers/settingController.js
+++ backend/controllers/settingController.js
@@ -3,7 +3,11 @@ import Setting from '../models/Setting.js'
 export const getSetting = async (req, res) => {
     try{
         const setting = await Setting.find()
-        res.status(200).json({data : setting})
+        const result = {};
+        setting.forEach(item => {
+            result[item.key] = item.value;
+        }); 
+        res.status(200).json({data : result})
     }catch(error){
         console.log(error)
     }
--- frontend-ecommerce/src/App.vue
+++ frontend-ecommerce/src/App.vue
@@ -1,11 +1,36 @@
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const title = ref("");
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:9887/api/setting/getsetting");
+    
+    // Ambil langsung title dari data
+    const settings = response.data.data;
+
+    if (settings.title) {
+      title.value = settings.title;
+      document.title = title.value;
+    } else {
+      console.warn("Title tidak ditemukan di response");
+    }
+
+  } catch (error) {
+    console.error("Gagal mengambil setting:", error);
+  }
+});
+</script>
+
 <template>
- 
-    <router-view />
- 
+  <router-view />
 </template>
 
 <style>
-html, body, #app {
+html,
+body,
+#app {
   margin: 0;
   padding: 0;
   width: 100%;
